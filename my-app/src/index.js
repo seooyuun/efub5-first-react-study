a54870cbbfd649c93ab4d632f68fa2dc04f3fa32
seooyuun <seoyuni2002@gmail.com>
2025-03-19 17:09:49 +0900

    실습 코드 추가

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import Library from './chapter_03/Library';
+import Library from "./chapter_03/Library";
+import Clock from "./chapter_04/Clock";
+import CommentList from "./chapter_05/CommentList";
+import NotificationList from "./chapter_06/NotificationList";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <Library />
-  </React.StrictMode>,
+    <NotificationList />
+  </React.StrictMode>
 );
+
+/*
+// chapter_05
+root.render(
+  <React.StrictMode>
+    <CommentList />
+  </React.StrictMode>
+);
+*/
+
+/*
+// chapter_04
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>
+  );
+}, 1000);
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
