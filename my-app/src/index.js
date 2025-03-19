6327ad38195abb146d041d78ae844d1f41b60a78
seooyuun <seoyuni2002@gmail.com>
2025-03-19 17:24:06 +0900

    실습 코드 추가

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -17,26 +17,6 @@ root.render(
   </React.StrictMode>
 );
 
-/*
-// chapter_05
-root.render(
-  <React.StrictMode>
-    <CommentList />
-  </React.StrictMode>
-);
-*/
-
-/*
-// chapter_04
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>
-  );
-}, 1000);
-*/
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
