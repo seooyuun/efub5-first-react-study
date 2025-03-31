dc509a52fc141e21d092acb9224f25974942f421
seooyuun <seoyuni2002@gmail.com>
2025-03-31 12:41:21 +0900

    feat: 섹션9-섹션11 실습 코드 추가

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -9,12 +9,15 @@ import Clock from "./chapter_04/Clock";
 import CommentList from "./chapter_05/CommentList";
 import NotificationList from "./chapter_06/NotificationList";
 import Accommodate from "./chapter_07/Accommodate";
+import ConfirmButton from "./chapter_08/ConfirmButton";
+import LandingPage from "./chapter_09/LandingPage";
+import AttendanceBook from "./chapter_10/AttendanceBook";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Accommodate />
+    <AttendanceBook />
   </React.StrictMode>
 );
 
